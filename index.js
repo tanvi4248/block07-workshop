5fb7a00e6135bbd74aaccd509b87d324be16fc3b
tanvi4248 <tanvi@GWTN141-10>
2023-11-06 11:46:00 -0600

    add javascript file

---
+++ index.js
@@ -0,0 +1,16 @@
+// This is javascript file.
+// Psudocode 
+// 1) Create a string that says "You have received this message because you have been chosen to open an important vault." 
+// 2) Create Three Variables with different name.
+// 3) Assign numbers to the Variables with different arithmetic operators.
+
+// Create string
+const message = "You have received this message because you have been chosen to open an important vault.Here is the secret combination:";
+// Create three variables sum, mul and sub.
+let sum = 5 + 5; // Addition of the  values.
+let mul = 8 * 5; // Multiplication of the values.
+let sub = 45 - 6; // Subtraction of the values.
+
+
+// Alert of the output.
+alert("Combination of the lock : "+ sum +" - "+ mul +" - "+ sub +" ");
